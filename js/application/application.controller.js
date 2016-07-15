898a55cf867fe3ec38bdc901f585e20e78f3e51a
rico nomenjanahary jeeferson <nomenajanaharyrico@gmail.com>
2016-07-15 14:38:30 +0300

    app.js

---
+++ js/application/application.controller.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Dev on 15/07/2016.
+ */
+angular.module('common.services')
+    .controller('applicationCtrl', function($scope,$rootScope, conf, $window, applicationFactory, ngProgressFactory) {
+        var strUserConnected = sessionStorage.getItem("UserConnected");
+        if (strUserConnected == null) {
+            $window.location.href = conf.site+'login.html';
+        }
+        $rootScope.userConnected = JSON.parse(strUserConnected);
+        $scope.applications = applicationFactory.getApplications().then(function (d) {
+            $scope.applications = d.data.records;
+            /*$scope.result = d.data.result.message;
+            $scope.Arraytask = [];
+            angular.forEach($scope.applications, function(value, key) {
+                $scope.Arraytask.push(value);
+            });*/
+        }, function (error) {
+            $scope.alerts.push({ msg: 'Une erreur est survenue', type: 'danger' });
+        });
+    });
