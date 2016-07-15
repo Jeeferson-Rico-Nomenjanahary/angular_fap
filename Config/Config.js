c9e9791050cf493a152575de1de55090c90da000
rico nomenjanahary jeeferson <nomenajanaharyrico@gmail.com>
2016-07-15 14:29:38 +0300

    initial commit

---
+++ Config/Config.js
@@ -0,0 +1,6 @@
+﻿angular.module('GlobalConfig', [])
+//***************************************************************************//
+.constant('conf', {
+    'entryPoint': 'http://10.2.150.239/API-Application/v1/',
+    'site': 'http://localhost/angular_fap/'
+});
